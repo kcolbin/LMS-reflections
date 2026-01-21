299657aef649f48d661f01a70bb8b39608a9f1b0
kcolbin <55267600+kcolbin@users.noreply.github.com>
2026-01-22 12:57:22 +1300

    Update reflection-questions.js

--- ACBE/reflection-questions.js
+++ ACBE/reflection-questions.js
@@ -1,20 +1,23 @@
 <script>
   // Single source of truth for ALL questions
   const REFLECTION_QUESTIONS = [
-    "What are two examples of sideways behavior that you commonly experience in your coaching?",
-    "Can you remember a time when someone showed they believed in you? Who was it / what did they do?",
-    "How did it feel?",
-    "What can you do to show belief in your players?",
-    "How did your coaches coach you? What did they do that was effective or ineffective for you?",
-    "How have you evolved since you started coaching?",
-    "Does Mulch’s reflection on his own journey resonate with you? Why or why not?",
-    "What’s an example of a mistake you made that you needed to own?",
-    "What makes it hard to own mistakes?",
-    "What’s an example of a time you didn’t feel like you belonged?",
-    "What’s an example of a time you did feel like you belonged?",
-    "What strategies for addressing sideways behaviour did you notice from Adrian and JT?",
-    "What lessons from sport have you transferred to other areas of your life?",
-    "How can you help your players set goals for themselves?",
-    "How can you help your players connect what they learn with you to their lives outside soccer?"
+{
+      title: "What are two examples of sideways behavior that you commonly experience in your coaching?",
+      subtext: "For example, athletes arriving late..."
+    },
+    {
+      title: "What, if any, strategies or tools do you currently have to approach sideways behavior?",
+    },
+    {
+      title: "How did it feel?"
+      // no subtext needed
+    },
+    {
+      title: "What can you do to show belief in your players?"
+    },
+    {
+      title: "How did your coaches coach you?",
+      subtext: "What did they do that was effective or ineffective for you?"
+    },
   ];
 </script>
