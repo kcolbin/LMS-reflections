123704ec91a960c99ccca7a02321c042694b842f
kcolbin <55267600+kcolbin@users.noreply.github.com>
2026-01-28 13:20:18 +1300

    Update reflection-questions.js

--- ACBE/reflection-questions.js
+++ ACBE/reflection-questions.js
@@ -1,105 +1,105 @@
   // Single source of truth for ALL questions
   const REFLECTION_QUESTIONS = [
 {
-      title: "What are two examples of challenging behavior that you commonly experience in your coaching?",
-      subtext: "For example, athletes not paying attention..."
-    },
-    {
-      title: "What, if any, strategies or tools do you currently have to address challenging behavior?",
-    },
-    {
-      title: "In the video you just watched, what did Amy, the coach, do to demonstrate empathy?"
-    },
-    {
-      title: "In the video you just watched, where did you feel empathy for Amy? Where did you feel empathy for Allie?"
-    },
-    {
-      title: "In your own words, how would you define empathy?",
-      subtext: "Hint: If you're struggling to write a complete sentence, write down five words that are important to your understanding of empathy."
-    },
-    {
-      title: "Here is the definition of empathy you gave earlier. Based on what you've seen in the video, is there anything you'd add to or change about your definition? Update it below.",
-    },
-    {
-      title: "A player isn't listening to instructions as you're trying to explain a new drill. What's one immediate-reaction guess as to why they aren't listening?",
-    },
-    {
-      title: "Assuming positive intent, what's another possible reason why they weren't listening?",
-    },
-    {
-      title: "What's an example of a time you were really proud of your coaching behavior?",
-    },
-    {
-      title: "What's an example of a time you weren't as proud of your coaching behavior?",
-    },
-    {
-      title: "Recognizing that (a) everybody stumbles and (b) the players are always watching, what does good role modeling look like in response to the moment you're not as proud of?",
-    },
-    {
-      title: "Sofia talks about having had strict coaches who yelled for most of her career. What kind of coaches have you had? What did you learn from them about the 'right' way to coach?",
-    },
-    {
-      title: "If you were a player, what impact would it have on you if your coach angrily yelled at you when you made mistakes?",
-    },
-    {
-      title: "If you were a player, what kind of coaching would lead you to perform at your absolute best?",
-    },
-    {
-      title: "If you were in Coach H's shoes, what would you do next?",
-    },
-    {
-      title: "Take another look at your answer below. Can you see some of the concepts and tools we covered in this module, like role modeling or Two Good Options, in your answer? Anything you want to add?",
-    },
-    {
-      title: "What skills did you notice Coach H using when she responded to Sofia?",
-    },
-    {
-      title: "What did you like about this approach?",
-    },
-    {
-      title: "What, if anything, would you do differently than Coach H?",
-    },
-    {
-      title: "If you were in Mulch's shoes, what would you do next?",
-    },
-    {
-      title: "Take another look at your answer below. Can you see some of the concepts and tools we covered in this module, like role modeling or Two Good Options, in your answer? Anything you want to add?",
-    },
-    {
-      title: "What skills did you notice Mulch using when he responded to his team?",
-    },
-    {
-      title: "What did you like about this approach?",
-    },
-    {
-      title: "What, if anything, would you do differently than Mulch?",
-    },
-    {
-      title: "JT talks about coaches as 'facilitators of the players' environment,' and says he gets input from players as young as five about what they would like to do at practice. How can you share power with your players so they feel ownership of their experience?",
-    },
-    {
-      title: "How might players' behavior shift when they feel their voice matters?",
-    },
-    {
-      title: "What did you notice about how Coach H held Sofia accountable for lying to her?",
-    },
-    {
-      title: "Coach H has clear standards of behavior, and at the same time only wants her players to be there if they want to be there. How can you hold players accountable while giving them ownership over their experience?",
-    },
-    {
-      title: "What messages have you been taught about conflict?",
-      subtext: "Is it good / bad / healthy / unhealthy? Something to be embraced? Something to be avoided?",
-    },
-    {
-      title: "Are there certain kinds of conflict you find more challenging than others? Which ones?",
-    },
-    {
-      title: "Understanding that conflict is inevitable, what, if anything, do you need to unlearn and re-learn to be able to effectively engage in healthy conflict?",
-    },
-    {
-      title: "Amy talks about not knowing what it's like to be deaf. What kinds of things might your players deal with that you haven't dealt with yourself? How can you show up with empathy in those situations?",
-    },
-    {
-      title: "How does empathy transform the experience for players?",
-    },
+  title: "What are two examples of challenging behavior that you commonly experience in your coaching?",
+  subtext: "For example, athletes not paying attention to instructions..."
+},
+{
+  title: "What, if any, strategies or tools do you currently have to address challenging behavior?"
+},
+{
+  title: "In the video you just watched, what did Amy, the coach, do to demonstrate empathy?"
+},
+{
+  title: "In the video you just watched, where did you feel empathy for Amy? Where did you feel empathy for Allie?"
+},
+{
+  title: "In your own words, how would you define empathy?",
+  subtext: "Hint: If you're struggling to write a complete sentence, write down five words that are important to your understanding of empathy."
+},
+{
+  title: "Here is the definition of empathy you gave earlier. Based on what you've seen in the video, is there anything you'd add to or change about your definition? Update it below."
+},
+{
+  title: "A player isn’t listening to instructions as you're trying to explain a new drill. What's your first assumption about why they aren't listening?"
+},
+{
+  title: "Assuming positive intent, what's another possible reason why they weren't listening?"
+},
+{
+  title: "What's an example of a time you were really proud of your coaching behavior?"
+},
+{
+  title: "What's an example of a time you weren't as proud of your coaching behavior?"
+},
+{
+  title: "Recognizing that (a) everybody stumbles and (b) the players are always watching, how can you model accountability for the moments you're not as proud of?"
+},
+{
+  title: "Sofia talks about having had strict coaches who yelled for most of her career. What kind of coaches have you had? What did you learn from them about the ‘right’ way to coach?"
+},
+{
+  title: "If you were a player, what impact would it have on you if your coach angrily yelled at you when you made mistakes?"
+},
+{
+  title: "If you were a player, what kind of coaching would lead you to perform at your absolute best?"
+},
+{
+  title: "What messages have you been taught about conflict?",
+  subtext: "Is it good / bad / healthy / unhealthy? Something to be embraced? Something to be avoided?"
+},
+{
+  title: "Are there certain kinds of conflict you find more challenging than others? Which ones?"
+},
+{
+  title: "Understanding that conflict is inevitable, what, if anything, do you need to unlearn and re-learn to be able to effectively engage in healthy conflict?"
+},
+{
+  title: "If you were in Coach H's shoes, what would you do next?"
+},
+{
+  title: "Take another look at your answer below. Can you see some of the concepts and tools we covered in this module, like role modeling or Two Good Options, in your answer? Anything you want to add?"
+},
+{
+  title: "What skills did you notice Coach H using when she responded to Sofia?"
+},
+{
+  title: "What did you like about this approach?"
+},
+{
+  title: "What, if anything, would you do differently than Coach H?"
+},
+{
+  title: "If you were in Mulch's shoes, what would you do next?"
+},
+{
+  title: "Take another look at your answer below. Can you see some of the concepts and tools we covered in this module, like role modeling or Two Good Options, in your answer? Anything you want to add?"
+},
+{
+  title: "What skills did you notice Mulch using when he responded to his team?"
+},
+{
+  title: "What did you like about this approach?"
+},
+{
+  title: "What, if anything, would you do differently than Mulch?"
+},
+{
+  title: "JT talks about coaches as 'facilitators of the players' environment,' and says he gets input from players as young as five about what they would like to do at practice. How can you share power with your players so they feel ownership of their experience?"
+},
+{
+  title: "How might players shift their behavior when they feel their voice matters?"
+},
+{
+  title: "What did you notice about how Coach H held Sofia accountable for lying to her?"
+},
+{
+  title: "Coach H has clear standards of behavior, and at the same time only wants her players to be there if they want to be there. How can you hold players accountable while giving them ownership over their experience?"
+},
+{
+  title: "Amy talks about not knowing what it's like to be deaf. What kinds of things might your players deal with that you haven't dealt with yourself? How can you show up with empathy in those situations?"
+},
+{
+  title: "How does empathy transform the experience for players?"
+}
   ];
